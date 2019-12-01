4e1f0edaf8f525f5e0f3c0a3a3628d7c43acb382
uiopak <44174807+uiopak@users.noreply.github.com>
2019-12-01 11:38:53 +0100

    Add project files.

---
+++ day_01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace day_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input");
+            int totalFuelPartOne = 0;
+            int totalFuelPartTwo = 0;
+            foreach (var line in input)
+            {
+                int mass = int.Parse(line);
+                int rocketFuel = ((mass / 3) - 2);
+                totalFuelPartOne += rocketFuel;
+                totalFuelPartTwo += rocketFuel;
+                while (true)
+                {
+                    rocketFuel = ((rocketFuel / 3) - 2);
+                    if (rocketFuel>0)
+                    {
+                        totalFuelPartTwo += rocketFuel;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"Total fuel part one: {totalFuelPartOne}");
+            Console.WriteLine($"Total fuel part two: {totalFuelPartTwo}");
+        }
+    }
+}
